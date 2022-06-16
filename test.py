0ff7f2a54cce18f989ce1c81f39e93f250047be9
gbnewb local <88551948+gbnewb@users.noreply.github.com>
2022-06-16 11:18:37 +0800

    Test Updated

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
-URL = 'https://dictionary.cambridge.org/dictionary/english/test'
-page = requests.get(URL, headers=headers)
+URL = 'https://dictionary.cambridge.org/dictionary/english/example'
+page = requests.get(URL, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'})
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+content = soup.find(id='page-content')
+ipa_ga = content.find_all('span', class_='ipa dipa lpr-2 lpl-1')
 
 with open('test/test.html', 'w', encoding='utf-8') as f:
     print(page.text, file=f)
+    [print(item) for item in ipa_ga]
 
