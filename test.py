61e333e1c5e3ae93fdf6140de9f8bde5c39600ba
gbnewb local <88551948+gbnewb@users.noreply.github.com>
2022-06-16 10:44:29 +0800

    Test added

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
-from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
 
-def web_ipa(word, site='Cambridge'):
-    match site:
-        case 'Cambridge':
-            webpage = requests.get(f'https://dictionary.cambridge.org/dictionary/english/{word}')
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
+URL = 'https://dictionary.cambridge.org/dictionary/english/test'
+page = requests.get(URL, headers=headers)
 
-def main():
-    word = input('>')
+with open('test/test.html', 'w', encoding='utf-8') as f:
+    print(page.text, file=f)
+
